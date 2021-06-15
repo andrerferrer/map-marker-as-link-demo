2ee3b0035e4668752fcefbb6ef5cf7a8667ea0bc
andre ferrer <andrerferrer@hotmail.com>
2021-06-15 12:39:13 -0300

    Make the marker clickable

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,5 @@
 import mapboxgl from 'mapbox-gl';
 
-// add this function
 const zoomMapToMarker = (map, marker) => {
   const bounds = new mapboxgl.LngLatBounds();
   bounds.extend([ marker.lng, marker.lat ])
@@ -10,24 +9,32 @@ const zoomMapToMarker = (map, marker) => {
 const addMarkerToMap = (map) => {
   const mapElement = document.getElementById('map');
 
-  // Take the marker from the view in `app/views/restaurants/show.html.erb`
-  const marker = JSON.parse(mapElement.dataset.marker);
+  const markerData = JSON.parse(mapElement.dataset.marker);
+  const marker = new mapboxgl.Marker()
+  .setLngLat([ markerData.lng, markerData.lat ])
+  .addTo(map);
 
-  new mapboxgl.Marker()
-    .setLngLat([ marker.lng, marker.lat ])
-    .addTo(map);
-  
-  zoomMapToMarker(map, marker) // add this line
+
+
+  const markerElement = marker.getElement(); // this gets the marker element. console.log it to see!
+  // add a nice link to go to
+  const linkToGo = `https://www.tripadvisor.com.br/Restaurant_Review-g303506-d6632629-Reviews-Amarelinho_Da_Gloria-Rio_de_Janeiro_State_of_Rio_de_Janeiro.html`
+  // wrap the marker element in a <a> tag
+  markerElement.innerHTML = `
+    <a class='add-some-style' href='${linkToGo}'>
+        ${markerElement.innerHTML}
+    </a>
+  `
+
+
+
+  zoomMapToMarker(map, markerData)
 }
-
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  // This if here is to ensure that we will only run the code
-  // IF we find this map element (which will be only in the Restaurants#Show)
   if (mapElement) {
-    // This is setting the access token as the one coming from app/views/restaurants/show.html.erb (line 11)
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
