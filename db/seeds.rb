068729370aa5cdf0f3307e1a500a8213e33376f8
andre ferrer <andrerferrer@hotmail.com>
2021-06-15 12:29:42 -0300

    change seeds to seed only amarelinho

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 puts "Create some Restaurants"
 
-Restaurant.create! name: "Amareleen", address: "Ladeira da Glória, 8 - Glória, Rio de Janeiro - RJ, 22211-120"
-Restaurant.create! name: "Verdeen", address: "Ladeira da Glória, 8 - Glória, Rio de Janeiro - RJ, 22211-120"
+Restaurant.create! name: "Amarelinho", address: "Ladeira da Glória, 8 - Glória, Rio de Janeiro - RJ, 22211-120"
 
 puts "Let's drink a beer"
 puts %{
